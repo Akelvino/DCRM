0cc8540fce8caa4f98e133b15398273daf1dbb98
Akelvino <vinho892@gmail.com>
2026-09-03 16:16:07 +0300

    added models to views

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Leadt, Agent, User
 
 # Register your models here.
+admin.site.register(Leadt)
+admin.site.register(Agent)
+admin.site.register(User)
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Leadt
 # Create your views here.
+def home_page(request):
+    leads = Leadt.objects.all()
+    context = {
+        "leads":leads
+    }
+    return render(request, 'leads/leads_list.html', context)
