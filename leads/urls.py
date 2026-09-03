ee7380b16336b08cb6c40685c4254c406cae4053
Akelvino <vinho892@gmail.com>
2026-09-03 16:47:43 +0300

    Finished creatinf the lead detail function

--- leads/urls.py
+++ leads/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home')
+    path('', views.home_page, name='home'),
+    path('details?/<int:pk>/', views.lead_detail, name='lead-detail'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -7,3 +7,11 @@ def home_page(request):
         "leads":leads
     }
     return render(request, 'leads/leads_list.html', context)
+
+def lead_detail(request, pk):
+    lead = Leadt.objects.get(id=pk)
+    context = {
+        'lead':lead
+    }
+
+    return render(request, 'leads/lead_details.html', context)
